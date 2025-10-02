8ba562be472c3946f6fd4d3eb35b37b56aa55f68
abu <mohameda.apxis@gmail.com>
2025-10-02 11:57:16 +0530

    header

--- src/public/js/common.js
+++ src/public/js/common.js
@@ -1,6 +1,6 @@
 // AOS about us
  AOS.init({
     duration: 1000,   // animation duration in ms
-    offset: 100,      // offset (px) from the original trigger point
-    once: true        // whether animation should happen only once
+    offset: 10,      // offset (px) from the original trigger point
+    once: false        // whether animation should happen only once
   });

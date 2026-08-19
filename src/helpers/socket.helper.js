1ed41cf260d1134e5adfcaa540a08cc1b37ff746
SabariRajan18 <sabarirajanm1018@gmail.com>
2026-08-19 23:38:47 +0530

    remove notification

--- src/helpers/socket.helper.js
+++ src/helpers/socket.helper.js
@@ -1,31 +1,32 @@
-import BookingModel from "../models/Bookings.js";
-import notification from '../helpers/notification.js';
-let socket;
-export const socketInitialize = (io) => {
-  socket = io;
-};
+// import BookingModel from "../models/Bookings.js";
+// import notification from '../helpers/notification.js';
+// let socket;
+// export const socketInitialize = (io) => {
+//   socket = io;
+// };
 
 export const alertDriversForNewBookings = async () => {
-  try {
-    const bookingDet = await BookingModel.find({ status: "Approve" });
-    socket.emit("new-booking", bookingDet);
-    const message = {
-      notification: {
-        title: "🚖 New Ride Request",
-        body: "A new booking is available now. Accept it before another driver does!",
-      },
-      topic: "allUsers",
-    };
+//   try {
+//     const bookingDet = await BookingModel.find({ status: "Approve" });
+//     socket.emit("new-booking", bookingDet);
+//     const message = {
+//       notification: {
+//         title: "🚖 New Ride Request",
+//         body: "A new booking is available now. Accept it before another driver does!",
+//       },
+//       topic: "allUsers",
+//     };
 
-    notification.messaging()
-      .send(message)
-      .then((response) => {
-        console.log("Successfully sent:", response);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  } catch (error) {
-    console.error({ alertDriversForNewBookings: error });
-  }
+//     notification.messaging()
+//       .send(message)
+//       .then((response) => {
+//         console.log("Successfully sent:", response);
+//       })
+//       .catch((err) => {
+//         console.log(err);
+//       });
+//   } catch (error) {
+//     console.error({ alertDriversForNewBookings: error });
+//   }
 };
+// 
